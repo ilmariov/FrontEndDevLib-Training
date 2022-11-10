835a953438a057bd5dbd169ca205b4907d6f1fb7
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-11-09 22:26:10 -0500

    test-app just created
    A test project to start training in react

---
+++ React/test-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const myElement = <h1>Hello JSX!</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
