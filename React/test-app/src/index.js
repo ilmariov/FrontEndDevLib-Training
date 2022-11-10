a2713e8196d7f589584ac891f2d09c72158b543f
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-11-09 22:46:31 -0500

    Update index.js

--- React/test-app/src/index.js
+++ React/test-app/src/index.js
@@ -8,6 +8,17 @@ const myElement = <h1>Hello JSX!</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
 
+const JSX = (
+    <div>
+        <h1>Hey Mario</h1>
+        <p>Let's render this to the DOM</p>
+    </div>
+);
+
+const test = ReactDOM.createRoot(document.getElementById('testing-this'));
+test.render(JSX);
+//ReactDOM.render(JSX, document.getElementById('testing-this'));
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
