d78282745f71c7107f3e88aa613cea3de03360ce
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-11-11 22:45:21 -0500

    lyfecycles methods tried

--- React/test-app/src/index.js
+++ React/test-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { PropTypes } from 'prop-types';
+//import { PropTypes } from 'prop-types';
 //import reportWebVitals from './reportWebVitals';
 
 //const myElement = <h1>Hello JSX!</h1>
@@ -170,7 +170,7 @@ ShoppingCart.defaultProps = {
     items: 0
 }*/
 
-class StatefulComponent extends React.Component {
+/*class StatefulComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -186,9 +186,9 @@ class StatefulComponent extends React.Component {
             </div>
         );
     }
-};
+};*/
 
-class MyComponent extends React.Component {
+/*class MyComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -209,8 +209,331 @@ class MyComponent extends React.Component {
             </div>
         );
     }
+};*/
+
+/*class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visibility: false
+        };
+        this.toggleVisibility = this.toggleVisibility.bind(this);
+    }
+    toggleVisibility() {
+        this.setState(state => {
+            if (state.visibility) {
+                return {visibility: false};
+            } else {
+                return {visibility: true};
+            }
+        });
+    }
+    render() {
+        if (this.state.visibility) {
+            return (
+            <div>
+                <button onClick={this.toggleVisibility}>Click Me</button>
+                <h1>Now you see me!</h1>
+            </div>
+            );
+        } else {
+            return (
+            <div>
+                <button onClick={this.toggleVisibility}>Click Me</button>
+            </div>
+            );
+        }
+    }
+}*/
+
+/*class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+    increment() {
+        this.setState(state => {
+            return {count: state.count + 1};
+        })
+    }
+    decrement() {
+        this.setState(state => {
+            return {count: state.count - 1};
+        })
+    }
+    reset() {
+        this.setState({
+            count: 0
+        });
+    }
+    render() {
+        return (
+            <div>
+                <button className='inc' onClick={this.increment}>Increment!</button>
+                <button className='dec' onClick={this.decrement}>Decrement!</button>
+                <button className='reset' onClick={this.reset}>Reset</button>
+                <h1>Current Count: {this.state.count}</h1>
+            </div>
+        );
+    }
+};*/
+
+/*class ControlledInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <input value={this.state.input} onChange={this.handleChange}></input>
+                <h4>Controlled Input:</h4>
+                <p>{this.state.input}</p>
+            </div>
+        );
+    }
+};*/
+
+/*class MyForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: '',
+            submit: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
+    handleSubmit(event) {
+        event.preventDefault()
+        this.setState({
+            submit: this.state.input
+        })
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input value={this.state.input} onChange={this.handleChange}/>
+                    <button type='submit'>Submit!</button>
+                </form>
+                <h1>{this.state.submit}</h1>
+            </div>
+        );
+    }
+}*/
+
+/*class MyApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'CamperBot'
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Navbar name={this.state.name}/>
+            </div>
+        );
+    }
 };
-test.render(<MyComponent/>)
+  
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+        <div>
+            <h1>Hello, my name is: {this.props.name}</h1>
+        </div>
+        );
+    }
+};*/
+
+/*class MyApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+            inputValue: event.target.value
+        });
+    }
+    render() {
+        return (
+            <div>
+                <GetInput input={this.state.inputValue} handleChange={this.handleChange}/>
+                <RenderInput input={this.state.inputValue}/>
+            </div>
+        );
+    }
+};
+  
+class GetInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h3>Get Input:</h3>
+                <input value={this.props.input} onChange={this.props.handleChange}/>
+            </div>
+        );
+    }
+};
+  
+class RenderInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h3>Input Render:</h3>
+                <p>{this.props.input}</p>
+            </div>
+        );
+    }
+};*/
+
+/*class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    UNSAFE_componentWillMount() {
+      console.log('This is something that will be mounted')
+    }
+    render() {
+      return <div />
+    }
+};*/
+
+/*class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeUsers: null
+        };
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                activeUsers: 1273
+            });
+        }, 2000);
+    }
+    render() {
+        return (
+            <div>
+                <h1>Active Users: {this.state.activeUsers}</h1>
+            </div>
+        );
+    }
+}*/
+
+/*class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ''
+        };
+        this.handleEnter = this.handleEnter.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyPress);
+    }
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyPress);
+    }
+    handleEnter() {
+        this.setState((state) => ({
+            message: state.message + 'You pressed the enter key! '
+        }));
+    }
+    handleKeyPress(event) {
+        if (event.keyCode === 13) {
+            this.handleEnter();
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+            </div>
+        );
+    }
+};*/
+
+class OnlyEvens extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('Should I update?');
+        if (nextProps.value % 3 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    componentDidUpdate() {
+        console.log('Component re-rendered.');
+    }
+    render() {
+        return <h1>{this.props.value}</h1>;
+    }
+}
+  
+class Controller extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        };
+        this.addValue = this.addValue.bind(this);
+    }
+    addValue() {
+        this.setState(state => ({
+            value: state.value + 1
+        }));
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.addValue}>Add</button>
+                <OnlyEvens value={this.state.value} />
+            </div>
+        );
+    }
+}
+
+test.render(<Controller/>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
