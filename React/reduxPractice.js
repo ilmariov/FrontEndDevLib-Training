c40472e59509c9d15c9944533695278a46509f99
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-11-13 14:25:18 -0500

    up to sending data to store

--- React/reduxPractice.js
+++ React/reduxPractice.js
@@ -21,7 +21,8 @@ const loginAction = () => {
 };*/
 
 // Using Switch statements
-const LOGIN = 'LOGIN';
+/******************************************************************************/
+/*const LOGIN = 'LOGIN';
 const LOGOUT = 'LOGOUT';
 const defaultState = {
     authenticated: false
@@ -50,4 +51,104 @@ const logoutUser = () => {
     return {
         type: LOGOUT
     }
+};*/
+
+//Register a store listener
+/**********************************************************************/
+/*const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+    switch(action.type) {
+        case ADD:
+            return state + 1;
+        default:
+            return state;
+    }
 };
+
+const store = Redux.createStore(reducer);
+
+let count = 0;
+
+function increment() {
+    return count++;
+}
+
+store.subscribe(increment);
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);*/
+
+//Combine multiple reducers
+/*************************************************************************/
+/*const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const counterReducer = (state = 0, action) => {
+    switch(action.type) {
+        case INCREMENT:
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+const authReducer = (state = {authenticated: false}, action) => {
+    switch(action.type) {
+        case LOGIN:
+            return {
+                authenticated: true
+            }
+        case LOGOUT:
+            return {
+                authenticated: false
+            }
+        default:
+            return state;
+    }
+};
+
+const rootReducer = Redux.combineReducers({
+    auth: authReducer,
+    count: counterReducer
+});
+
+const store = Redux.createStore(rootReducer);*/
+
+//Send action data to the store
+/***********************************************************************/
+const ADD_NOTE = 'ADD_NOTE';
+
+const notesReducer = (state = 'Initial State', action) => {
+    switch(action.type) {
+        case ADD_NOTE:
+            return action.text;
+        default:
+            return state;
+    }
+};
+
+const addNoteText = (note) => {
+  return {
+    type: ADD_NOTE,
+    text: note
+  }
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
+
+//Use Middleware to Handle Asynchronous Actions
+/*************************************************************************/
